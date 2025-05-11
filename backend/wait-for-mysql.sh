aafd1eef6f9fa5365da87bb66685675f9a2d37ff
Julien <juliendurand77140@gmail.com>
2025-05-11 16:25:47 +0200

    working with docker and kubernetes

---
+++ backend/wait-for-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "⏳ Attente de MySQL à l'adresse $MYSQL_HOST:$MYSQL_PORT..."
+
+# Par défaut
+: "${MYSQL_HOST:=mysql}"
+: "${MYSQL_PORT:=3306}"
+
+# Boucle jusqu'à ce que MySQL réponde
+until nc -z "$MYSQL_HOST" "$MYSQL_PORT"; do
+  echo "⏳ MySQL n'est pas encore prêt - attente..."
+  sleep 2
+done
+
+echo "✅ MySQL est prêt !"
